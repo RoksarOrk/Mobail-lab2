6451cbbe2364de1ccec4e76be30acbc2733dfd89
RoksarOrk <106882024+RoksarOrk@users.noreply.github.com>
2025-04-17 02:25:01 +0500

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,36 @@
+import { LongPressEvent } from 'react-native-maps';
+
+export interface MarkerData {
+    id: string;
+    latitude: number;
+    longitude: number;
+    created_at?: string;
+    images: ImageData[];
+}
+
+export interface ImageData {
+    id: string;
+    uri: string;
+}
+
+export interface MarkerRouteParams {
+    id: string;
+}
+
+export interface MapProps {
+    markers: MarkerData[];
+    onLongPress: (event: LongPressEvent) => void;
+    onMarkerPress: (markerId: string) => void;
+}
+
+export interface DatabaseContextType {
+    addMarker: (latitude: number, longitude: number) => Promise<string>;
+    deleteMarker: (id: string) => Promise<void>;
+    getMarkers: () => Promise<MarkerData[]>;
+    addImage: (markerId: string, uri: string) => Promise<string>;
+    deleteImage: (id: string) => Promise<void>;
+    getMarkerImages: (markerId: string) => Promise<ImageData[]>;
+    isLoading: boolean;
+    error: Error | null;
+    getMarkerById: (id: string) => Promise<MarkerData>;
+}
